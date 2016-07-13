d3ae99f2d4d0c2c846648501499c466513d6c449
Eric Rasche <rasche.eric@gmail.com>
2016-07-13 15:20:19 +0000

    argparseify

--- go_parse.py
+++ go_parse.py
@@ -1,15 +1,16 @@
+#!/usr/bin/env python
+import os
 import json
+import argparse
 import sys
 
-with open("go.obo") as handle:
+def parseObo(handle, count=-1, outdir="."):
     json_objects = []
     go_term = {}
     term_start = False
+    output_terms = 0
 
     for num, line in enumerate(handle):
-        if num > 63:
-            sys.exit()
-
         if not line.strip() and term_start:
             json_objects.append(go_term)
             # print re.split(r"['\"](.*?)['\"]", go_term['def'])
@@ -28,8 +29,11 @@ with open("go.obo") as handle:
 
             go_term['db_xref'] = d
 
-            with open('%s.json' % go_term['id'], 'w') as out:
+            with open(os.path.join(outdir, '%s.json' % go_term['id']), 'w') as out:
                 json.dump(go_term, out)
+                output_terms += 1
+                if count != -1 and output_terms > count:
+                    sys.exit()
 
             go_term = {}
             term_start = False
@@ -40,3 +44,13 @@ with open("go.obo") as handle:
 
         elif line.startswith('[Term]'):
             term_start = True
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('obo', type=file, help='Path to OBO file')
+    parser.add_argument('--count', type=int, help="Maximum number of OBO terms to dump before quitting. Useful in testing")
+    parser.add_argument('--outdir', help="output directory")
+    args = parser.parse_args()
+
+    parseObo(args.obo, count=args.count, outdir=args.outdir)
